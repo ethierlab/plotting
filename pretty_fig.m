b653ee91d5333b8db48d17238a14d69aa6ca8d2b
maximehuotlavoie <maxime.huot-lavoie.1@ulaval.ca>
2018-06-27 14:52:16 -0400

    Changed Formatting for Both plotCE.m and prettyfig.m

--- pretty_fig.m
+++ pretty_fig.m
@@ -7,12 +7,12 @@ else
 end
 
 set(current_axes,'Box','Off',...
-        'FontName','Arial','FontSize',16,'FontWeight','Bold',...
-        'TickDir','Out','LineWidth',2);
+        'FontName','Arial','FontSize',12,'FontWeight','Bold',...
+        'TickDir','Out','LineWidth',1.5);
 
 % change line colors to the jet colormap
 lines     = get(current_axes,'Children');
 for i = 1:numel(lines)
-    set(lines(i),'LineWidth',2);
+    set(lines(i),'LineWidth',1.5);
  
 end
