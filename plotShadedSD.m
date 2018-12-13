2ba11e6bbf8d17a8c00f3a8f0f94677b3fd5efe9
Christian Ethier <christian.ethier@gmail.com>
2018-12-13 13:12:32 -0500

    Update plotShadedSD.m

--- plotShadedSD.m
+++ plotShadedSD.m
@@ -1,15 +1,22 @@
-function plotShadedSD(varargin)
-% plotShadedSD(timeframe,Data,SD) or plotShadedSD(fh,timeframe,Data,SD)
+function fh = plotShadedSD(varargin)
+% plotShadedSD(timeframe,Data,SD,[colors_cellarray]) or plotShadedSD(fh,timeframe,Data,SD,[colors_cellarray])
+col_str = {};
 
 if ishandle(varargin{1})
     fh = varargin{1};
     timeframe = varargin{2};
     Data = varargin{3};
     SD = varargin{4};
+    if nargin>4
+        col_str = varargin{5};
+    end
 else
     timeframe = varargin{1};
     Data = varargin{2};
     SD = varargin{3};
+    if nargin>3
+        col_str = varargin{4};
+    end
     fh = figure;
 end
 
@@ -18,16 +25,25 @@ xIdx  = [1:numpts numpts:-1:1];
 xarea = timeframe(xIdx);
 % xarea = xIdx;
 
-
-plotCE(fh,timeframe,Data);
+ax = axes(fh);
 hold on;
+
+if isempty(col_str)
+    plot(ax,timeframe,Data);
+else
+    for i = 1:size(Data,2)
+        plot(ax,timeframe,Data(:,i),col_str{i});
+    end
+end
+
 lines = flipud(get(gca,'Children'));
 for i = 1:size(Data,2)
+    
     ytop = Data(:,i) + SD(:,i);
     ybot = Data(:,i) - SD(:,i);
     yarea = [ytop; ybot(end:-1:1)];
     patch(xarea,yarea,lines(i).Color,'LineStyle','none','FaceAlpha',0.3);
-%     area(xarea,yarea,'Facecolor',lines(i).Color,'LineStyle','none','FaceAlpha',0.3);
+    %     area(xarea,yarea,'Facecolor',lines(i).Color,'LineStyle','none','FaceAlpha',0.3);
 end
 
 % flip order of lines
